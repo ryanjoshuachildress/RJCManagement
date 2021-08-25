a2f242bdc371272a2111f5b8b63bf67402599bb7
ryanjoshuachildress <ryanjoshuachildress@gmail.com>
2021-08-25 12:14:49 -0700

    Mid Commit

--- app/src/main/java/com/ryanjoshuachildress/rjcmanagement/MainActivity.kt
+++ app/src/main/java/com/ryanjoshuachildress/rjcmanagement/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ryanjoshuachildress.rjcmanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Button
 import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
